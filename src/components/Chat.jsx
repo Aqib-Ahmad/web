dfd96b994a86c4fd24949fe3559f563b3397dc4b
Aqib ahmad khan <aqibkhan0396@gmail.com>
2025-08-12 08:59:26 +0530

    adding data in Db

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,41 +1,56 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { createSocketConnection } from "../utils/Socket";
 import { useSelector } from "react-redux";
-
+import axios from "axios";
+import { BASE_URL } from "../utils/constant";
 const Chat = () => {
   const { targetUserId } = useParams();
   const user = useSelector((store) => store.user);
   const userId = user?._id;
-  console.log(targetUserId);
   const [messages, setMessages] = useState([]);
   const [newMessages, setNewMessages] = useState("");
+  const socketRef = useRef(null);
+
+  const fetchChatMessages = async () => {
+    const chat = await axios.get(BASE_URL + "/chat/" + targetUserId, {
+      withCredentials: true,
+    });
+    const chatMessages = chat?.data.messages.map((msg) => {
+      return {
+        firstName: msg.senderId?.firstName || "Unknown",
+        senderId: msg.senderId?._id,
+        text: msg.text,
+      };
+    });
+    setMessages(chatMessages);
+  };
   useEffect(() => {
-    if (!userId) {
-      return;
-    }
-    // as soon page loads  , the socket connection is made and jooinchat event is emitted
+    if (!userId || !targetUserId) return;
+    fetchChatMessages();
+  }, [userId, targetUserId]);
+  useEffect(() => {
+    if (!userId) return;
     const socket = createSocketConnection();
+    socketRef.current = socket;
+
     socket.emit("joinChat", {
-      firstName: user.firstName,
       userId,
       targetUserId,
-      text: newMessages,
     });
-    console.log({ firstName: user.firstName, text: newMessages });
 
     socket.on("messageReceivd", ({ firstName, text }) => {
-      console.log(firstName + " from " + text);
       setMessages((messages) => [...messages, { firstName, text }]);
     });
-    // disconnect after connecting
+
     return () => {
       socket.disconnect();
     };
   }, [userId, targetUserId]);
+
   const sendMessage = () => {
-    const socket = createSocketConnection();
-    socket.emit("sendMessage", {
+    if (!newMessages.trim()) return;
+    socketRef.current.emit("sendMessage", {
       firstName: user.firstName,
       userId,
       targetUserId,
@@ -52,11 +67,11 @@ const Chat = () => {
       <div className="card m-auto card-border bg-base-100 w-96 h-96 border-black">
         <div className="card-body relative">
           <div
-            className="overflow-y-auto h-72 pr-2"
-            style={{ maxHeight: "18rem" }} // adjust as needed
+            className="overflow-y-auto overflow-x-hidden h-72 pr-2 w-full"
+            style={{ maxHeight: "18rem" }}
           >
             {messages.map((msg, index) => {
-              const isSender = msg.firstName === user.firstName;
+              const isSender = msg.senderId === userId;
               return (
                 <div key={index}>
                   <div
